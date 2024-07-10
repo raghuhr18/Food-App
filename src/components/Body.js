b1d71089f5f4d5e69443e96180e1972c8900ff53
raghuhr18 <raghuhr18@yahoo.com>
2024-07-10 20:14:16 +0530

    implementation of search functionality

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,14 +1,35 @@
+import { useState } from "react";
 import { restaurantList } from "../../constants";
 import RestaurantCard from "./RestaurantCard"
 
+function searchClicked(filteredRestaurants, searchItem) {
+    const filterData = filteredRestaurants.filter((restaurant) =>
+        restaurant?.info?.name?.includes(searchItem)
+    )
+    return filterData;
+ } 
+
 
 const Body = () => {
+
+    const [searchItem, setSearchItem] = useState("")
+    const [filteredRestaurants , setFilteredRestaurants] = useState(restaurantList)
+
     return (
-      <div className="restaurant-list">
-        {restaurantList.map((restaurant) => 
-          <RestaurantCard {...restaurant.info} key={restaurant.info.id}/>
-        )}
-    </div>
+    <>
+        <div>
+            <input type="Search" placeholder="Search Item" value={searchItem} onChange={(e)=>{setSearchItem(e.target.value)}}/>
+            <button onClick={() => {
+                const data = searchClicked(restaurantList, searchItem)
+                setFilteredRestaurants(data)
+                }}>Search</button>
+        </div>
+        <div className="restaurant-list">
+            {filteredRestaurants.map((restaurant) => 
+            <RestaurantCard {...restaurant.info} key={restaurant.info.id}/>
+            )}
+        </div>
+    </>
   )
 }
 export default Body;
